54e55baf7339a408d13f1f55a7a73f0638441da9
Nehabrahmbhatt <97364317+Nehabrahmbhatt@users.noreply.github.com>
2022-01-10 13:37:49 +0000

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -4,7 +4,8 @@ public class FirstProgram {
 
     public static void main(String[] args ) {
         System.out.println("Neha Brahmbhatt ");
-        System.out.println("SoftWare Testing Point");
+        System.out.println("SoftWare Testing Manual Point");
+        
 
 
 
